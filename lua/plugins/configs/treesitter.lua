d7d92eeaa658b089d393a80f0953d5b25c320bd8
Tyrone Hills <mobw4u@gmail.com>
2026-02-06 08:58:05 +0200

    Initial commit: My custom NvChad setup

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  ensure_installed = { "lua", "vim", "vimdoc", "python" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
---
+++ plugins.lua
@@ -0,0 +1,114 @@
+local plugins = {
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust" },
+                auto_install = true,
+
+                highlight = {
+                    enable = true,
+                },
+
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        -- Start Selection
+                        init_selesction = "<Leader>ss",
+                        -- Start Increment
+                        node_incremental = "<Leader>si",
+                        -- Selection Scope or Scope
+                        scope_incremental = "<Leader>sc",
+                        -- Selection Decrement
+                        node_decremental = "<Leader>sd",
+                    },
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+
+                        -- Automatically jump forward to textobj, similar to targets.vim
+                        lookahead = true,
+
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                            -- nvim_buf_set_keymap) which plugins like which-key display
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            -- You can also use captures from other query groups like `locals.scm`
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        -- You can choose the select mode (default is charwise 'v')
+                        --
+                        -- Can also be a function which gets passed a table with the keys
+                        -- * query_string: eg '@function.inner'
+                        -- * method: eg 'v' or 'o'
+                        -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                        -- mapping query_strings to modes.
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V', -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        -- If you set this to `true` (default is `false`) then any textobject is
+                        -- extended to include preceding or succeeding whitespace. Succeeding
+                        -- whitespace has priority in order to act similarly to eg the built-in
+                        -- `ap`.
+                        --
+                        -- Can also be a function which gets passed a table with the keys
+                        -- * query_string: eg '@function.inner'
+                        -- * selection_mode: eg 'v'
+                        -- and should return true or false
+                        include_surrounding_whitespace = true,
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        event = "VeryLazy",
+        opts = function()
+            return require "custom.configs.null-ls"
+        end,
+    },
+    {
+        "mfussenegger/nvim-dap",
+        config = function(_, _)
+            require("core.utils").load_mappings("dap")
+        end
+    },
+    
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "custom.configs.lspconfig"
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "clangd",
+                "clang-format",
+                "codelldb",
+                "black",
+                "mypy",
+                "pyright",
+                "ruff-lsp",
+                "marksman",
+                "gopls",
+                "rust-analyzer",
+                "debugpy",
+                "lua-language-server",
+            }
+        }
+    }
+}
+return plugins
